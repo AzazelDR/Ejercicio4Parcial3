1d087086c567214d1fa1412d7b29a6925bcebfda
AzazelDR - Axel Reyes <49660378+AzazelDR@users.noreply.github.com>
2021-10-18 19:12:33 -0600

    Parcial

---
+++ js/Ejercicio4.js
@@ -0,0 +1,30 @@
+var a = parseInt(prompt("Ingrese el numero 1"));
+if (a % 2 == 0) {
+    document.write("<h1>El numero es par</h1>")
+} else {
+    document.write("<h1>El numero es impar</h1>")
+}
+var b = parseInt(prompt("Ingrese el numero 2"));
+if (b % 2 == 0) {
+    document.write("<h1>El numero es par</h1>")
+} else {
+    document.write("<h1>El numero es impar</h1>")
+}
+var c = parseInt(prompt("Ingrese el numero 3"));
+if (c % 2 == 0) {
+    document.write("<h1>El numero es par</h1>")
+} else {
+    document.write("<h1>El numero es impar</h1>")
+}
+var d = parseInt(prompt("Ingrese el numero 4"));
+if (d % 2 == 0) {
+    document.write("<h1>El numero es par</h1>")
+} else {
+    document.write("<h1>El numero es impar</h1>")
+}
+var e = parseInt(prompt("Ingrese el numero 5"));
+if (e % 2 == 0) {
+    document.write("<h1>El numero es par</h1>")
+} else {
+    document.write("<h1>El numero es impar</h1>")
+}
